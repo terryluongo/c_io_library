fe593b01281fba22fcf3676a1897d8805e98ca36
Terry Luongo <trluongo@optimum.net>
2023-10-04 21:53:24 -0400

    added header file and deleted extraneous comments in test.c

--- test.c
+++ test.c
@@ -8,31 +8,7 @@
 #include <stdlib.h>
 
 #define BYTES_PER_ITERATION 10240
-/*
-opendir(3)  - open directory
-readdir(3) - read directory 
-closedir(3) - close directory 
-getopt(3) - get option character from command line argument list 
-stat(2) -  The stat() function obtains information about the file pointed to by path | probably for the -l flag 
-strftime(3) - format date and time
-getpwnam(3) - something with passwords
-localtime(3) - The functions ctime(), gmtime(), and localtime() all take as an argument a time value representing the time in seconds since the Epoch (00:00:00 UTC, January 1, 1970; see time(3)).
-     When encountering an error, these functions return NULL and set errno to an appropriate value.
 
-
-1. pick directory we want
-2. opendir
-3. read contents pointer by pointer
-4. use stat to get info
-5. repeat until error or done
-6. close directory
-
-later:
-use strftime, localtime, getpwnam for -l flag
-
-
-
-*/
 int
 main(int argc, char *argv[])
 {
