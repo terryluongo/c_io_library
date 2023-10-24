c990554aac187f8b433dbbc22f99c29e7ef63f3b
Terry Luongo <trluongo@optimum.net>
2023-10-24 14:51:48 -0400

    working on myread

--- myio.c
+++ myio.c
@@ -1,6 +1,8 @@
 #include "myio.h"
 
-#define buf_max 10
+
+
+#define buf_max 11
 myfile *myopen(const char *pathname, int flags,mode_t mode) {
 	int fd;	
 	fd = open(pathname,flags,mode);
@@ -22,19 +24,37 @@ ssize_t myread(myfile *file, void *buf, size_t count) {
 	//printf("myread called\n");
 	//printf("roffset + count %lu\n",file->roffset + count);
 	//printf("%d\n",buf_max);
-	
+	//file->rbufend = buf_max;	
 	size_t returnCount = count;
 	int buf_amount = 0;
+
+	if(file->rbufend == -1) {
+		return 0;
+	}
 	//first thing we have to check is if this call will go over the buf_max
 	//we have to check this before the simple case of a first read because we may have to call read to get more
 	if (file->roffset + count == buf_max) {
 		buf_amount = count;
 		memcpy(buf,file->rbuf + file->roffset,count);
 		file->roffset=0;
+		if(file->rbufend < buf_max) {
+
+			file->rbufend = -1;
+		}
 		return returnCount;
 	}
-	else if (file->roffset + count > buf_max) {
+	else if (file->rbufend && file->roffset + count > buf_max) {
 		//printf("trigger flush\n");
+		if(file->rbufend < buf_max) {
+			printf("eof\n");
+			printf("%d\n",file->roffset);
+			printf("%d\n",file->rbufend);
+			memcpy(buf,file->rbuf + file->roffset, file->rbufend);
+			returnCount = file->rbufend - file->roffset;
+			file->rbufend = -1;
+			printf("returnCount: %d\n",returnCount);
+			return returnCount;
+		}
 		buf_amount = buf_max - file->roffset;
 		// copy whatever is left into the buffer before restarting 
 		//printf("%s\n",file->rbuf+file->roffset);
@@ -52,7 +72,6 @@ ssize_t myread(myfile *file, void *buf, size_t count) {
 	
 	//copy into myread buffer the properly advanced amount from read buf
 	memcpy((char *) buf + buf_amount,file->rbuf + file->roffset,count - buf_amount);
-
 	//advance beginning of buffer pointer
 	file->roffset += count - buf_amount;
 
@@ -65,7 +84,11 @@ ssize_t myread(myfile *file, void *buf, size_t count) {
 		returnCount = file->rbufend;
 	}
 	//retreat end of buffer pointer
-	file->rbufend -= returnCount;
+	//file->rbufend -= returnCount;
+	//printf("buf_amount: %d ",buf_amount);
+	//printf("roffset: %d ",file->roffset);
+	//printf("rbufend: %d ",file->rbufend);
+	//printf("returnCount: %d\n",returnCount);
 	return returnCount;
 }
 ssize_t mywrite(myfile *file, const void *buf, size_t count) {
